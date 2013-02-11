c67527c321cf39d27d6d3e9dfca826fabc0522cf
Alex Eftimie <alex@eftimie.ro>
2013-02-11 16:22:30 +0200

    Rename functions to bwt

--- functions.php
+++ functions.php
@@ -1,25 +1,25 @@
 <?php
-function abw_addmenus() {
+function bwt_addmenus() {
 	register_nav_menus(
 		array(
 			'main_nav' => 'The Main Menu',
 		)
 	);
 }
-add_action( 'init', 'abw_addmenus' );
+add_action( 'init', 'bwt_addmenus' );
 
-function abw_nav_class($classes, $item){
+function bwt_nav_class($classes, $item){
 	 if (in_array('current-menu-item', $classes) or in_array('current-page-ancestor', $classes))
 		$classes[] = 'active';
      return $classes;
 }
-add_filter('nav_menu_css_class' , 'abw_nav_class' , 10 , 2);
+add_filter('nav_menu_css_class' , 'bwt_nav_class' , 10 , 2);
 
 function bwt_widgets_init() {
 	register_sidebar( array(
 		'name' => __( 'Main Sidebar', 'bwt' ),
 		'id' => 'sidebar-1',
-		'description' => __( 'Appears on posts and pages except the optional Front Page template, which has its own widgets', 'twentytwelve' ),
+		'description' => __( 'Appears on posts and pages except the optional Front Page template, which has its own widgets', 'bwt' ),
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget' => '</aside>',
 		'before_title' => '<h3 class="widget-title">',
